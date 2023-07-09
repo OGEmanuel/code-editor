9f8816ef37df814b568812e50455bf2476b63efc
OGEmanuel <emanuolumide@mail.com>
2023-07-09 22:38:01 +0100

    final commit

--- main.js
+++ main.js
@@ -1,16 +1,69 @@
 const textarea = document.querySelectorAll('textarea');
+const html = document.getElementById('html');
+const css = document.getElementById('css');
+const js = document.getElementById('js');
+const btnHtml = document.getElementById('tabs-html');
+const btnCss = document.getElementById('tabs-css');
+const btnJs = document.getElementById('tabs-js');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
 
 const onChange = () => {
-  let html = document.getElementById('html').value;
-  let css = document.getElementById('css').value;
-  let js = document.getElementById('js').value;
   let result = document.getElementById('result');
 
-  result.contentDocument.body.innerHTML = html + '<style>' + css + '</style>';
-  result.contentWindow.eval(js);
+  result.contentDocument.body.innerHTML =
+    html.value + '<style>' + css.value + '</style>';
+  result.contentWindow.eval(js.value);
 };
 
 for (let i = 0; i < textarea.length; i++) {
   const code = textarea[i];
   code.addEventListener('keyup', onChange);
 }
+
+if (!btnHtml.classList.contains('hidden')) {
+  btnHtml.style.borderColor = '#ff4d00';
+}
+
+const switchToHtmlTab = () => {
+  one.classList.remove('hidden');
+  two.classList.add('hidden');
+  three.classList.add('hidden');
+
+  if (!btnHtml.classList.contains('hidden')) {
+    btnHtml.style.borderColor = '#ff4d00';
+    btnCss.style.borderColor = '#000';
+    btnJs.style.borderColor = '#000';
+  }
+};
+
+btnHtml.addEventListener('click', switchToHtmlTab);
+
+const switchToCssTab = () => {
+  one.classList.add('hidden');
+  two.classList.remove('hidden');
+  three.classList.add('hidden');
+
+  if (!btnCss.classList.contains('hidden')) {
+    btnHtml.style.borderColor = '#000';
+    btnCss.style.borderColor = '#025cf7';
+    btnJs.style.borderColor = '#000';
+  }
+};
+
+btnCss.addEventListener('click', switchToCssTab);
+
+const switchToJsTab = () => {
+  one.classList.add('hidden');
+  two.classList.add('hidden');
+  three.classList.remove('hidden');
+
+  if (!btnJs.classList.contains('hidden')) {
+    btnHtml.style.borderColor = '#000';
+    btnCss.style.borderColor = '#000';
+    btnJs.style.borderColor = '#eafa05';
+  }
+};
+
+btnJs.addEventListener('click', switchToJsTab);
